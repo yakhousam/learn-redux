214bec5aee7eee605915b1faae79ad366e1e21fd
yakhousam <42283612+yakhousam@users.noreply.github.com>
2021-09-04 12:29:02 +0100

    used action creators

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "./redux/actionCreator";
 
 function App() {
   const dispatch = useDispatch();
   const state = useSelector((state) => state);
 
   // you will notice that i'm handling the constraints here not on my reducer
-  const handleIncrement = () => state < 10 && dispatch({ type: "INCREMENT" });
-  const handleDecrement = () => state > 0 && dispatch({ type: "DECREMENT" });
+  const handleIncrement = () => state < 10 && dispatch(increment());
+  const handleDecrement = () => state > 0 && dispatch(decrement());
   return (
     <div className='App'>
       <h1>state = greater than 0 and less than 10</h1>
