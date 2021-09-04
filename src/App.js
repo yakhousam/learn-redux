e143207656465091734a66d0ca362edf6899d9f0
yakhousam <42283612+yakhousam@users.noreply.github.com>
2021-09-04 09:21:44 +0100

    added counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+
+  // you will notice that i'm handling the constraints here not on my reducer
+  const handleIncrement = () => state < 10 && dispatch({ type: "INCREMENT" });
+  const handleDecrement = () => state > 0 && dispatch({ type: "DECREMENT" });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>state = greater than 0 and less than 10</h1>
+      <p>state: {state}</p>
+      <button onClick={handleIncrement}>Increment</button>{" "}
+      <button onClick={handleDecrement}>Decrement</button>
     </div>
   );
 }
